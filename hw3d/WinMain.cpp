bbf80065afb7960c2ccc5ceebe5dad67ec95496a
dushengsheng <dushengsheng.51@bytedance.com>
2022-03-23 23:39:25 +0800

    save

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+#include "WindowsMessageMap.h"
+#include <sstream>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static WindowsMessageMap mm;
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
+	switch (msg)
+	{
+		case WM_CLOSE:
+			PostQuitMessage(69);
+			break;
+		case WM_KEYDOWN:
+			if (wParam == 'F')
+			{
+				SetWindowText(hWnd, "Respects");
+			}
+			break;
+		case WM_KEYUP:
+			if (wParam == 'F')
+			{
+				SetWindowText(hWnd, "Dangerfield");
+			}
+			break;
+		case WM_CHAR:
+		{
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+		}
+		break;
+		case WM_LBUTTONDOWN:
+		{
+			POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText(hWnd, oss.str().c_str());
+		}
+			break;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance, 
+	_In_opt_ HINSTANCE hPrevInstance, 
+	_In_ LPSTR lpCmdLine, 
+	_In_ int nShowCmd)
+{
+	const auto pClassName = "hw3dbutts";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIcon = nullptr;
+	RegisterClassEx(&wc);
+
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	ShowWindow(hWnd, SW_SHOW);
+
+	MSG msg;
+	BOOL gResult = FALSE;
+	while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+		return -1;
+	else
+		return msg.wParam;
+}
