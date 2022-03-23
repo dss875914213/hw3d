0a8a5669551cd39a3e11530965e9c072d5ae8e86
dushengsheng <dushengsheng.51@bytedance.com>
2022-03-23 23:58:17 +0800

    出现窗口，但是是空白的

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,7 +1,37 @@
 #include <windows.h>
 
-int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance, 
+	_In_opt_ HINSTANCE hPrevInstance, 
+	_In_ LPSTR lpCmdLine, 
+	_In_ int nShowCmd)
 {
-	while (true);
-	return 0;
+	const char* className = "hw3d";
+	// register window class
+	WNDCLASS wc;
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = NULL;
+	wc.hCursor = NULL;
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = className;
+	RegisterClass(&wc);
+
+	// create window instance
+	HWND hWnd = CreateWindow(className, 
+		"hw3d T2 Test", 
+		WS_CAPTION, 
+		200, 200, 400, 300, 
+		NULL,
+		NULL, 
+		hInstance,
+		NULL);
+
+	// show the window
+	ShowWindow(hWnd, SW_SHOW);
+	while (1);
 }
